791599c6d607b4fb2b3eb9b1fddc0e27d7bca0ec
SharmilaGalidevara <sgal0028@student.monash.edu>
2026-06-30 21:06:13 +0800

    Retrieve and display player results from localStorage

--- js/result.js
+++ js/result.js
@@ -1,1 +1,14 @@
+const name =
+localStorage.getItem("playerName");
 
+const score =
+localStorage.getItem("score");
+
+const time =
+localStorage.getItem("timeTaken");
+
+document.getElementById("resultName").innerText =
+name;
+
+document.querySelector(".score").innerText =
+`${score} / 40`;
