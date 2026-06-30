4a2340e1901072d8eddb01368aa160c0d981446c
SharmilaGalidevara <sgal0028@student.monash.edu>
2026-06-30 21:34:39 +0800

    Refactor storage.js to change storage key and variable

--- js/storage.js
+++ js/storage.js
@@ -1,30 +1,17 @@
-// =========================
-// STORAGE MANAGER
-// =========================
+const STORAGE_KEY = "bgakl_leaderboard";
 
-const STORAGE_KEY = "bgakl_quiz_history";
-
-// Save result
 function saveResult(result) {
 
-    let history = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    let data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 
-    history.push(result);
+    data.push(result);
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(history));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 
 }
 
-// Get leaderboard
 function getLeaderboard() {
 
     return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-
-}
-
-// Clear (optional)
-function clearHistory() {
-
-    localStorage.removeItem(STORAGE_KEY);
 
 }
